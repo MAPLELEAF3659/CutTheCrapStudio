eb433d4674a18c15c11c5bd8ee61289bc79f1d0b
MAPLELEAF3659 <ig910425@gmail.com>
2021-11-11 12:57:39 +0800

    update index.js

--- js/index.js
+++ js/index.js
@@ -1,35 +1,34 @@
 var controller = new ScrollMagic.Controller();
+var scene;
 
-$(function(){
-    var scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 25})
-    .setPin("#head1")
-    .addIndicators()
-    .addTo(controller);
+$(function () {
+    scene = new ScrollMagic.Scene({ triggerElement: "#trigger1", duration: 25 })
+        .setPin("#head1")
+        .addIndicators()
+        .addTo(controller);
 
-    scene =  new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 50})
-    .setPin("#head2")
-    .addIndicators()
-    .addTo(controller);
+    scene = new ScrollMagic.Scene({ triggerElement: "#trigger1", duration: 50 })
+        .setPin("#head2")
+        .addIndicators()
+        .addTo(controller);
 
-    scene =  new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 100})
-    .setPin("#head3")
-    .addIndicators()
-    .addTo(controller);
+    scene = new ScrollMagic.Scene({ triggerElement: "#trigger1", duration: 100 })
+        .setPin("#head3")
+        .addIndicators()
+        .addTo(controller);
 
-    scene =  new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 200})
-    .setPin("#logo")
-    .addIndicators()
-    .addTo(controller);
+    scene = new ScrollMagic.Scene({ triggerElement: "#trigger1", duration: 200 })
+        .setPin("#logo")
+        .addIndicators()
+        .addTo(controller);
 
-    scene =  new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 600})
-    .setPin("#hand")
-    .addIndicators()
-    .addTo(controller);
-
-    scene =  new ScrollMagic.Scene({triggerElement: "#trigger2"})
+    scene = new ScrollMagic.Scene({ triggerElement: "#trigger1", duration: 600 })
+        .setPin("#hand")
+        .addIndicators()
+        .addTo(controller);
+        
+    scene = new ScrollMagic.Scene({ triggerElement: "#trigger2", duration: 1 })
     .setPin("#slogan")
-    .setTween("#slogan", 0.5, {backgroundColor: "green", scale: 2.5})
     .addIndicators()
     .addTo(controller);
-
 });
