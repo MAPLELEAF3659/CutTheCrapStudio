bc061f90f0ecf3e973265953ac4cb6c37f280d2c
MAPLELEAF3659 <ig910425@gmail.com>
2021-11-11 23:00:37 +0800

    add scroll down to play slogan animation. fix animation z-index problem.

--- js/index.js
+++ js/index.js
@@ -1,36 +1,43 @@
 var controller = new ScrollMagic.Controller();
 var scene;
 
-$(function () {
+$(function() {
     scene = new ScrollMagic.Scene({ triggerElement: "#trigger1", duration: 25 })
         .setPin("#head1")
+        .offset(350)
         .addIndicators()
         .addTo(controller);
 
     scene = new ScrollMagic.Scene({ triggerElement: "#trigger1", duration: 50 })
         .setPin("#head2")
+        .offset(350)
         .addIndicators()
         .addTo(controller);
 
     scene = new ScrollMagic.Scene({ triggerElement: "#trigger1", duration: 100 })
         .setPin("#head3")
+        .offset(350)
         .addIndicators()
         .addTo(controller);
 
     scene = new ScrollMagic.Scene({ triggerElement: "#trigger1", duration: 200 })
         .setPin("#logo")
+        .offset(350)
         .addIndicators()
         .addTo(controller);
 
     scene = new ScrollMagic.Scene({ triggerElement: "#trigger1", duration: 600 })
         .setPin("#hand")
+        .offset(350)
         .addIndicators()
         .addTo(controller);
 
     scene = new ScrollMagic.Scene({
-        triggerElement: "#trigger2",
-        triggerHook: 0.15})
-        .setTween(tl)
+            triggerElement: "#trigger2",
+            duration: 0,
+            triggerHook: 0.8
+        })
+        .setTween("#slogan-title", 1, { opacity: 1 })
         .addIndicators()
         .addTo(controller);
 });
