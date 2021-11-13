74f54b8a5dd9aac3387ca4323fd8325fa5e2f191
MAPLELEAF3659 <ig910425@gmail.com>
2021-11-13 12:45:22 +0800

    add recentWork slideshow. add logo image. remove scrollmagic indicator.

--- js/index.js
+++ js/index.js
@@ -5,31 +5,26 @@ $(function() {
     scene = new ScrollMagic.Scene({ triggerElement: "#trigger1", duration: 25 })
         .setPin("#head1")
         .offset(350)
-        .addIndicators()
         .addTo(controller);
 
     scene = new ScrollMagic.Scene({ triggerElement: "#trigger1", duration: 50 })
         .setPin("#head2")
         .offset(350)
-        .addIndicators()
         .addTo(controller);
 
     scene = new ScrollMagic.Scene({ triggerElement: "#trigger1", duration: 100 })
         .setPin("#head3")
         .offset(350)
-        .addIndicators()
         .addTo(controller);
 
     scene = new ScrollMagic.Scene({ triggerElement: "#trigger1", duration: 200 })
         .setPin("#logo")
         .offset(350)
-        .addIndicators()
         .addTo(controller);
 
     scene = new ScrollMagic.Scene({ triggerElement: "#trigger1", duration: 600 })
         .setPin("#hand")
         .offset(350)
-        .addIndicators()
         .addTo(controller);
 
     scene = new ScrollMagic.Scene({
@@ -38,6 +33,5 @@ $(function() {
             triggerHook: 0.8
         })
         .setTween("#slogan-title", 1, { opacity: 1 })
-        .addIndicators()
         .addTo(controller);
 });
