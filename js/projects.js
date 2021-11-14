0001746692750beead3b3b5c3e6064e8e225c523
MAPLELEAF3659 <ig910425@gmail.com>
2021-11-14 15:56:31 +0800

    add project page title animation.

--- js/projects.js
+++ js/projects.js
@@ -1,23 +1,40 @@
 var controller = new ScrollMagic.Controller();
-/*const circles = document.querySelectorAll('.main-circle');
+var scene;
 
-const setCircleInitXPosition = (circle) => {
-    const START_WIDTH = 24;
-    const windowWidth = window.innerWidth;
-    const circleLeftPosition = Math.random() * windowWidth - START_WIDTH;
-    circle.style.left = `${circleLeftPosition}px`;
-};
+mainAreaHeight = $('.main-title-text').height(); // container height
+mainAreaWidth = $('.main-title').width(); // container width
 
-circles.forEach(setCircleInitXPosition);*/
+$(function() {
+    $(".main-circle").each(function() {
+        randomtop = Math.random() * mainAreaHeight + 100;
+        randomleft = Math.random() * mainAreaWidth - 24;
+        $(this).css({
+            'top': randomtop,
+            'left': randomleft
+        });
 
-$(".ani-area").each(function() {
-    var scene = new ScrollMagic.Scene({
-            triggerElement: this,
-            offset: -100
-        })
-        .setPin("#head")
-        .addIndicaters()
-        .addTo(controller);
+        // var speed = $(this).data('speed');
+        // var translate = ($(window).scrollTop() - speed) / $(window).height() * 100;
+        // $(this).css({ transform: 'translateY(' + translate + '%)' });
+    });
+
+    $(".ani-area").each(function() {
+        scene = new ScrollMagic.Scene({
+                triggerElement: this,
+                duration: 100
+            })
+            .setPin(".ani-area-head")
+            .addIndicaters()
+            .addTo(controller);
+    });
 });
+
+$(window).scroll(function() {
+    $(".main-circle").each(function() {
+        $(this).css({
+            'margin-top': -$(window).scrollTop()
+        });
+    });
+})
 
 objectFitImages();
