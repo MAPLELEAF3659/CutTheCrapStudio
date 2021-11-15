fe30f83a0a5d1fce73c73d9e99eb8ea9d4d5d472
MAPLELEAF3659 <ig910425@gmail.com>
2021-11-15 23:04:32 +0800

    add sideNav bg animation. update css

--- js/sideNav.js
+++ js/sideNav.js
@@ -2,8 +2,10 @@ $(".sideNav").sidebar({ speed: 400 });
 
 $(".header-nav").click(function() {
     $(".sideNav").trigger("sidebar:open");
+    $(".sideNav__bg").addClass("fadeIn");
 });
 
 $(".sideNav-btn-close").click(function() {
     $(".sideNav").trigger("sidebar:close");
+    $(".sideNav__bg").removeClass("fadeIn");
 });
