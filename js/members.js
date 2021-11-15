9a3828bfd2e84d336a6ad44d7f738459536433fb
MAPLELEAF3659 <ig910425@gmail.com>
2021-11-15 18:48:54 +0800

    add members page line animation. update index,project page.

---
+++ js/members.js
@@ -0,0 +1,23 @@
+var controller = new ScrollMagic.Controller();
+var scene;
+
+var lineHeight = $(".draw-ani-line").height();
+
+$(function() {
+    scene = new ScrollMagic.Scene({
+            triggerElement: "#trigger",
+            duration: 1650
+        })
+        .setPin("#hand")
+        .on("progress", function(e) {
+            if (e.progress == 1) {
+                $("#hand").addClass('fade-out');
+            } else {
+                $("#hand").removeClass('fade-out');
+            }
+            $(".draw-ani-line").css({
+                'height': e.progress.toFixed(3) * lineHeight
+            });
+        })
+        .addTo(controller);
+});
