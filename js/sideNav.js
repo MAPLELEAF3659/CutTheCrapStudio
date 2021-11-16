2c8f54f4f42b61f974f241e201304da6c38df75c
MAPLELEAF3659 <ig910425@gmail.com>
2021-11-16 19:07:58 +0800

    update text rwd

--- js/sideNav.js
+++ js/sideNav.js
@@ -1,6 +1,8 @@
 $(".sideNav").sidebar({ speed: 400 });
+$(".sideNav").css("opacity", 0);
 
 $(".header-nav").click(function() {
+    $(".sideNav").css("opacity", 1);
     $(".sideNav").css("z-index", 99);
     $(".sideNav").trigger("sidebar:open");
     $(".sideNav__bg").addClass("fadeIn");
