f1742e491e3fab335aa2e91e0b428169e0e2dfc5
MAPLELEAF3659 <ig910425@gmail.com>
2021-11-16 16:53:52 +0800

    update sideNav

--- js/sideNav.js
+++ js/sideNav.js
@@ -1,6 +1,7 @@
 $(".sideNav").sidebar({ speed: 400 });
 
 $(".header-nav").click(function() {
+    $(".sideNav").css("z-index", 99);
     $(".sideNav").trigger("sidebar:open");
     $(".sideNav__bg").addClass("fadeIn");
 });
@@ -8,4 +9,12 @@ $(".header-nav").click(function() {
 $(".sideNav-btn-close").click(function() {
     $(".sideNav").trigger("sidebar:close");
     $(".sideNav__bg").removeClass("fadeIn");
+});
+
+$(".sideNav").on("sidebar:opened", function() {
+    $(".sideNav").css("z-index", 99);
+});
+
+$(".sideNav").on("sidebar:closed", function() {
+    $(".sideNav").css("z-index", -99);
 });
