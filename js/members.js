4cc540017d6fbb706e9223fda54cf13c7664162d
MAPLELEAF3659 <ig910425@gmail.com>
2021-11-16 16:18:12 +0800

    update members page.

--- js/members.js
+++ js/members.js
@@ -1,13 +1,14 @@
 var controller = new ScrollMagic.Controller();
 var scene;
 
+$(".draw-ani-line").css("height", $(".members-area").height());
 var lineHeight = $(".draw-ani-line").height();
 $(".draw-ani-line").css("height", "0px");
 
 $(function() {
     scene = new ScrollMagic.Scene({
-            triggerElement: "#trigger",
-            duration: 1650
+            triggerElement: "#trigger1",
+            duration: lineHeight
         })
         .setPin("#hand")
         .on("progress", function(e) {
@@ -20,5 +21,59 @@ $(function() {
                 'height': e.progress.toFixed(3) * lineHeight
             });
         })
+        .addTo(controller);
+
+    scene = new ScrollMagic.Scene({
+            triggerElement: "#trigger2",
+            duration: 0,
+            triggerHook: 0.8
+        })
+        .offset(300)
+        .setTween("#item2", 1, { opacity: 1 })
+        .addIndicators()
+        .addTo(controller);
+
+    scene = new ScrollMagic.Scene({
+            triggerElement: "#trigger3",
+            duration: 0,
+            triggerHook: 0.8
+        })
+        .offset(300)
+        .setTween("#item3", 1, { opacity: 1 })
+        .addIndicators()
+        .addTo(controller);
+
+    scene = new ScrollMagic.Scene({
+            triggerElement: "#trigger4",
+            duration: 0,
+            triggerHook: 0.8
+        })
+        .offset(300)
+        .setTween("#item4", 1, { opacity: 1 })
+        .addIndicators()
+        .addTo(controller);
+
+    scene = new ScrollMagic.Scene({
+            triggerElement: "#trigger5",
+            duration: 0,
+            triggerHook: 0.8
+        })
+        .on("enter leave", function(e) {
+            if (e.type == "enter" ? true : false) {
+                $(".fourDims-area-image-box.hor").addClass("isAni");
+                $(".fourDims-area-image-box.ver").addClass("isAni");
+                $(".fourDims-area-image-box.head").addClass("isAni");
+                $(".fourDims-area-image-box.bg").addClass("isAni");
+                $(".fourDims-area p").addClass("isAni");
+            } else {
+                $(".fourDims-area-image-box.hor").removeClass("isAni");
+                $(".fourDims-area-image-box.ver").removeClass("isAni");
+                $(".fourDims-area-image-box.head").removeClass("isAni");
+                $(".fourDims-area-image-box.bg").removeClass("isAni");
+                $(".fourDims-area p").removeClass("isAni");
+            }
+        })
+        .offset(500)
+        .addIndicators()
         .addTo(controller);
 });
