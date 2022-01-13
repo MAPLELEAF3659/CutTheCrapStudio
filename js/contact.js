ddefad63dd7a0bd0926f0c6193592cf8a5910702
MAPLELEAF3659 <ig910425@gmail.com>
2022-01-13 15:05:05 +0800

    add firebase function. add contact page icon scroll triggers.

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,57 @@
+var controller = new ScrollMagic.Controller();
+var scene;
+
+$(function() {
+    scene = new ScrollMagic.Scene({
+            triggerElement: "#trigger1",
+            duration: 0,
+            triggerHook: 0.8
+        })
+        .offset(100)
+        .setTween("#item1", 1, { opacity: 1 })
+        .on("start", function(e) {
+            phoneIconAnimation.goToAndPlay(0);
+        })
+        .addTo(controller);
+
+    scene = new ScrollMagic.Scene({
+            triggerElement: "#trigger2",
+            duration: 0,
+            triggerHook: 0.8
+        })
+        .offset(100)
+        .setTween("#item2", 1, { opacity: 1 })
+        .on("start", function(e) {
+            emailIconAnimation.goToAndPlay(0);
+        })
+        .addTo(controller);
+
+    scene = new ScrollMagic.Scene({
+            triggerElement: "#trigger3",
+            duration: 0,
+            triggerHook: 0.8
+        })
+        .offset(100)
+        .setTween("#item3", 1, { opacity: 1 })
+        .on("start", function(e) {
+            addressIconAnimation.goToAndPlay(0);
+        })
+        .addTo(controller);
+});
+
+$(window).scroll(function() {
+    $("#title-image1").css({
+        'margin-top': -$(window).scrollTop() * 0.25
+    });
+    $("#title-image2").css({
+        'margin-top': -$(window).scrollTop() * 0.4
+    });
+    $("#title-image3").css({
+        'margin-top': -$(window).scrollTop() * 0.55
+    });
+});
+
 var phoneIconAnimation = bodymovin.loadAnimation({
-    // animationData: { /* ... */ },
     container: document.getElementById('icon-phone'), // required
     path: 'image/contactUs/phone.json', // required
     renderer: 'svg', // required
@@ -7,7 +59,6 @@ var phoneIconAnimation = bodymovin.loadAnimation({
     autoplay: true, // optional
 });
 var addressIconAnimation = bodymovin.loadAnimation({
-    // animationData: { /* ... */ },
     container: document.getElementById('icon-address'), // required
     path: 'image/contactUs/address.json', // required
     renderer: 'svg', // required
@@ -15,7 +66,6 @@ var addressIconAnimation = bodymovin.loadAnimation({
     autoplay: true, // optional
 });
 var emailIconAnimation = bodymovin.loadAnimation({
-    // animationData: { /* ... */ },
     container: document.getElementById('icon-email'), // required
     path: 'image/contactUs/mail.json', // required
     renderer: 'svg', // required
@@ -33,14 +83,20 @@ $("#info-title-address").mouseover(function() {
     addressIconAnimation.goToAndPlay(0);
 });
 
-$(window).scroll(function() {
-    $("#title-image1").css({
-        'margin-top': -$(window).scrollTop() * 0.25
+if (!firebase.apps.length) {
+    firebase.initializeApp({
+        apiKey: "AIzaSyCsriGcqSA9rW52K7RLN3-ATn5QZ0E7r6s",
+        authDomain: "ctcstudio-84f9a.firebaseapp.com",
+        projectId: "ctcstudio-84f9a"
     });
-    $("#title-image2").css({
-        'margin-top': -$(window).scrollTop() * 0.4
-    });
-    $("#title-image3").css({
-        'margin-top': -$(window).scrollTop() * 0.55
-    });
-});
+} else {
+    firebase.app(); // if already initialized, use that one
+}
+
+var db = firebase.firestore();
+// db.collection("message").add({
+//     userName: "Consumable",
+//     userEmail: "ctcstudio@gmail.com",
+//     userPhone: "0912345678",
+//     userMessage: "Hi"
+// })
